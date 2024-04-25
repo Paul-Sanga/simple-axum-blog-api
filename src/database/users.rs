41a161a664818133afda03a5b4f5aa8f4efe44af
Paul Sanga <paulsanganyamawi@gmail.com>
2024-04-25 09:57:25 +0000

    Adding tracing to log activities to the console

--- src/database/users.rs
+++ src/database/users.rs
@@ -8,7 +8,7 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
     pub first_name: String,
-    pub last_name: String,
+    pub last_name: Option<String>,
     #[sea_orm(unique)]
     pub email: String,
     pub password: String,
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,15 +1,20 @@
 use axum::{http::Method, routing::get, Router};
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 pub fn create_routes() -> Router {
-
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
 
-    
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     Router::new()
-    .route("/", get(||async{String::from("I am Phantom Vasploit")}))
-    .layer(cors)
+        .route("/", get(|| async { String::from("I am Phantom Vasploit") }))
+        .layer(cors)
+        .layer(TraceLayer::new_for_http())
 }
