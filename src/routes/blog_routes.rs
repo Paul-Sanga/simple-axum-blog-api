a898c4cbd2e2c71ccc28490c890bec653d1c2c97
Paul Sanga <paulsanganyamawi@gmail.com>
2024-04-25 12:50:47 +0000

    Get one blog

--- src/routes/blog_routes.rs
+++ src/routes/blog_routes.rs
@@ -1,8 +1,8 @@
-use axum::{Extension, Json};
-use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
-use serde::Deserialize;
+use axum::{extract::Path, http::StatusCode, Extension, Json};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
+use serde::{Deserialize, Serialize};
 
-use crate::database::blog;
+use crate::database::blog::{self, Entity as Blog};
 
 #[derive(Deserialize, Debug)]
 pub struct RequestBlog {
@@ -24,4 +24,30 @@ pub async fn create_blog(
     dbg!(&result);
 }
 
-pub async fn get_blog() {}
+#[derive(Serialize)]
+pub struct ResponseBlog{
+    id: i32,
+    title: String,
+    detail: String
+}
+
+pub async fn get_blog(
+    Path(blog_id): Path<i32>,
+    Extension(database_conection): Extension<DatabaseConnection>,
+) -> Result<Json<ResponseBlog>, StatusCode> {
+    let blog = Blog::find_by_id(blog_id).one(&database_conection).await;
+    if let Ok(blog) = blog {
+        if let Some(blog) = blog {
+            let response_data = ResponseBlog{
+                id: blog.id,
+                title: blog.title,
+                detail: blog.details
+            };
+            Ok(Json(response_data))
+        } else {
+            Err(StatusCode::NOT_FOUND)
+        }
+    } else {
+        Err(StatusCode::BAD_REQUEST)
+    }
+}
